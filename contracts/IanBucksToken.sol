73b742a1617bfdf59b92a080b3d31b9d2b2671d1
Tyler Wasson <twasson94@gmail.com>
2019-10-04 01:07:13 -0400

    Delegated Transfer functions built out

--- contracts/IanBucksToken.sol
+++ contracts/IanBucksToken.sol
@@ -12,7 +12,14 @@ contract IanBucksToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping( address => uint256 ) public balanceOf;
+    mapping(address => mapping(address=> uint256)) public allowance;
 
     constructor ( uint256 _initialSupply ) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -20,10 +27,28 @@ contract IanBucksToken {
     }
 
     function transfer( address _to, uint256 _value ) public returns ( bool success ) {
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value, 'Not enough tokens');
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve( address _spender, uint256 _value ) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from], 'Not enough tokens');
+        require(_value <= allowance[_from][msg.sender], 'Trying to send more than was approved');
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
